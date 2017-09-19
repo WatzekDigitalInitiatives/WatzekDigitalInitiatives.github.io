bc9a63d6af4e0953022f1e8c5ae6f9c1a63b2d39
Jeremy McWilliams <jeremym@lclark.edu>
2017-09-19 16:51:25 -0700

    made images local

---
+++ timeline/test.php
@@ -0,0 +1,26 @@
+<?php
+$row = 1;
+if (($handle = fopen("src.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo "$c : ".$data[$c] . "<br />\n";
+            if ($c==11){
+              $url=$data[$c];
+              $d=explode("/",$url);
+              $im=array_pop($d);
+
+              copy($url, "images/$im");
+
+            }
+
+
+
+
+        }
+    }
+    fclose($handle);
+}
+ ?>
